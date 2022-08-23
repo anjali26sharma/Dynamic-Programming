a5552b7ae21b486d821df9040d2845a32aa0f8d4
anjali26sharma <79824809+anjali26sharma@users.noreply.github.com>
2022-08-23 15:49:59 +0530

    Create Partition Equal Subset.cpp

---
+++ Partition Equal Subset.cpp
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/partition-equal-subset-sum/
+
+class Solution {
+public:
+    bool helper(vector<int>& nums, int sum, int n, vector<vector<bool>> &t)
+    {
+        for(int i =0; i<n+1; i++)
+            for(int j=0; j<sum+1; j++)
+            {   if(i==0)
+                    t[i][j]=false;
+                 if(j==0)
+                     t[i][j]=true;
+            }
+        
+        for(int i =1; i<n+1; i++)
+            for(int j =1; j<sum+1; j++)
+            {
+                if(nums[i-1]<=j)
+                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
+                else if(nums[i-1]>j)
+                    t[i][j] = t[i-1][j];
+                
+            }
+        return t[n][sum];
+        
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(),sum =0;
+        for(int i =0; i<n; i++)
+        {
+            sum+= nums[i];
+        }
+        if(sum%2 !=0)
+            return false;
+        else
+        {   sum = sum/2;
+            vector<vector<bool>> t(n+2,vector<bool>(sum+2, NULL));
+            return helper(nums,sum,n,t);
+        }
+    }
+};
