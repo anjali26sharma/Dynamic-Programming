a6ea77fc6b2ff2f601287ca9501d87715f669b09
anjali26sharma <79824809+anjali26sharma@users.noreply.github.com>
2022-10-07 14:19:22 +0530

    Binary Search

---
+++ LIS 3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     
+    
+    int LIS(vector<int>& nums) {
+        int len;
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        len = 1;
+        for(int i = 1; i< nums.size();i++)
+        {
+            if(nums[i]>temp.back())
+            {   temp.push_back(nums[i]);
+                len++; 
+            }
+            else
+            { auto ind = lower_bound(temp.begin(),temp.end(),nums[i]);
+              temp[ind - temp.begin()] = nums[i];
+            }
+        }
+        return len;
+    }
+};
